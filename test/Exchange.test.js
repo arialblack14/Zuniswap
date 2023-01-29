ee4d969e7cd5a99a2bc35fa3f32d12a8f00f3956
arialblack14 <tsirasalexis@yahoo.gr>
2023-01-29 23:09:42 +0200

    Fix hardhat-waffle failing while running test

--- test/Exchange.test.js
+++ test/Exchange.test.js
@@ -1,57 +1,69 @@
 require("@nomiclabs/hardhat-waffle");
 const { expect } = require("chai");
-const { ethers } = require("hardhat");
 
 const toWei = (value) => ethers.utils.parseEther(value.toString());
+
+const fromWei = (value) =>
+  ethers.utils.formatEther(
+    typeof value === "string" ? value : value.toString()
+  );
+
 const getBalance = ethers.provider.getBalance;
 
 describe("Exchange", () => {
-    let token;
-    let exchange;
-    let owner;
-    let user;
+  let owner;
+  let user;
+  let exchange;
 
-    beforeEach(async () => {
-        [owner, user] = await ethers.getSigners();
+  beforeEach(async () => {
+    [owner, user] = await ethers.getSigners();
 
-        const Token = await ethers.getContractFactory("Token");
-        token = await Token.deploy("Token", "TKN", toWei(1e6)); 
-        await token.deployed();
+    const Token = await ethers.getContractFactory("Token");
+    token = await Token.deploy("Token", "TKN", toWei(1000000));
+    await token.deployed();
 
-        const Exchange = await ethers.getContractFactory("Exchange");
-        exchange = await Exchange.deploy(token.address);
-        await exchange.deployed();
-    })
+    const Exchange = await ethers.getContractFactory("Exchange");
+    exchange = await Exchange.deploy(token.address);
+    await exchange.deployed();
+  });
 
-    it("is deployed", async () => {
-        expect(await exchange.deployed.to.equal(exchange));
-    })
+  it("is deployed", async () => {
+    expect(await exchange.deployed()).to.equal(exchange);
+  });
 
-    describe("addLiquidity", async () => {
-        it("adds liquidity", async () => {
-            await token.approve(exchange.address, toWei(200));
-            await exhange.addLiquidity(toWei(200), { value: toWei(100) });
-    
-            expect(await getBalance(exchange.address)).to.equal(toWei(100));
-            expect(await exchange.getReserve()).to.equal(toWei(200));
-        })
-    })
+  describe("addLiquidity", async () => {
+    it("adds liquidity", async () => {
+      await token.approve(exchange.address, toWei(200));
+      await exchange.addLiquidity(toWei(200), { value: toWei(100) });
 
-    describe("getPrice", async () => {
-        it("returns correct prices", async () => {
-            await token.approve(exchange.address, toWei(200));
-            await exchange.addLiquidity(toWei(2000), { value: toWei(1000) });
+      expect(await getBalance(exchange.address)).to.equal(toWei(100));
+      expect(await exchange.getReserve()).to.equal(toWei(200));
+    });
 
-            const tokenReserve = await exchange.getReserve();
-            const etherReserve = await getBalance(exchange.address);
+    it("allows zero amounts", async () => {
+      await token.approve(exchange.address, 0);
+      await exchange.addLiquidity(0, { value: 0 });
 
-            // ETH per token
-            expect(
-                (await exchange.getPrice(etherReserve, tokenReserve).toString()
-            ).to.eq("0.5"));
+      expect(await getBalance(exchange.address)).to.equal(0);
+      expect(await exchange.getReserve()).to.equal(0);
+    });
+  });
 
-            // token per ETH
-            expect(await exchange.getPrice(etherReserve, tokenReserve).toString().to.eq("2"));
-        })
-    })
+  describe("getPrice", async () => {
+    it("returns correct prices", async () => {
+      await token.approve(exchange.address, toWei(2000));
+      await exchange.addLiquidity(toWei(2000), { value: toWei(1000) });
+  
+      const tokenReserve = await exchange.getReserve();
+      const etherReserve = await getBalance(exchange.address);
+  
+      // ETH per token
+      expect(
+        (await exchange.getPrice(etherReserve, tokenReserve)).toString()
+      ).to.eq("0.5");
+  
+      // token per ETH
+      expect(await exchange.getPrice(tokenReserve, etherReserve)).to.eq(2);
+    });
+  });
 })
